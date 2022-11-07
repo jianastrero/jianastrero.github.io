df8879f7a95d34f752892d8337999ba79c628d73
Jian James Astrero <jianjamesastrero@gmail.com>
2022-11-07 17:12:20 +0800

    fix error from import

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
-import PocketBase from '/js/pocketbase/pocketbase.es.mjs';
+console.log("hello world")
 
-const client = new PocketBase('https://jianastrero-api.herokuapp.com');
+import PocketBase from './pocketbase/pocketbase.es.mjs'
+
+const client = new PocketBase('https://jianastrero-api.herokuapp.com')
 
 // alternatively you can also fetch all records at once via getFullList:
-const records = await client.records.getFullList('bio', {sort: '-created',});
+const records = await client.records.getFullList('bio', {sort: '-created',})
 
 console.log("records", records)
