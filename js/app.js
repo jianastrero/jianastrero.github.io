0c8a1dc67e4e527d0c64ca8e36119f07f15e35f0
Jian James Astrero <jianjamesastrero@gmail.com>
2022-11-07 17:08:55 +0800

    test pocketbase

---
+++ js/app.js
@@ -0,0 +1,8 @@
+import PocketBase from '/js/pocketbase/pocketbase.es.mjs';
+
+const client = new PocketBase('https://jianastrero-api.herokuapp.com');
+
+// alternatively you can also fetch all records at once via getFullList:
+const records = await client.records.getFullList('bio', {sort: '-created',});
+
+console.log("records", records)
